453aafced3f72a049bb83158dba34cdc018d994b
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2020-01-04 00:21:52 +0200

    Add headers to ajax request

--- src/common/js/contactForm.js
+++ src/common/js/contactForm.js
@@ -4,6 +4,9 @@ $('#form')
     $.ajax({
       type: 'POST',
       url: 'https://api.lynx.org.ua/callback',
+      headers: {
+        Accept: 'application/x.lynx.v1+json',
+      },
       success(responsedata) {
         // process on data
         console.log(`${'got response as ' + ' '}${responsedata} `);
